4a66b6f41e368550d2a159b83a3e35999c38e6df
Venigalla Shamanth Chowdary <venigallashamanthchowdary@gmail.com>
2024-10-19 05:19:16 +0530

    commits

---
+++ script.js
@@ -0,0 +1,10 @@
+// static/js/script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Example function to handle a button click
+    const button = document.getElementById('rankButton');
+    button.addEventListener('click', function() {
+        alert('Ranking process started!');
+        // You can add more functionality here, like sending data to your Flask app
+    });
+});
